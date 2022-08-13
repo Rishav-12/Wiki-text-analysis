ea5144ec0d1dc43b7a0b2d1612b54af8363fb924
Rishav Mitra <rishavmitra10@gmail.com>
2022-08-13 12:42:38 +0000

    :bug: fixed error due to empty input

--- main.py
+++ main.py
@@ -10,27 +10,30 @@ st.write("""
 """)
 
 term = st.text_input("Enter a term to look up: ")
-result = wikipedia.search(term, results=1)[0]
-page = wikipedia.page(result)
+if term == "":
+    st.write("Please enter a valid search term!")
+else:
+    result = wikipedia.search(term, results=1)[0]
+    page = wikipedia.page(result)
 
-subheading = f"Wordcloud for the Wikipedia page {page.title}"
-st.write(subheading)
-content = page.content
+    subheading = f"Wordcloud for the Wikipedia page {page.title}"
+    st.write(subheading)
+    content = page.content
 
-frequent_words = find_most_frequent_words(content)
+    frequent_words = find_most_frequent_words(content)
 
-words_to_display = ""
+    words_to_display = ""
 
-for i in frequent_words:
-    if i[0].isalnum():
-        words_to_display += f"- {i[0]}: {i[1]}\n"
+    for i in frequent_words:
+        if i[0].isalnum():
+            words_to_display += f"- {i[0]}: {i[1]}\n"
 
-st.markdown(words_to_display)
+    st.markdown(words_to_display)
 
-wordcloud = make_wordcloud(content)
-fig = plt.figure(figsize=(30, 30))
-plt.imshow(wordcloud)
-plt.axis('off')
-plt.tight_layout(pad=0)
+    wordcloud = make_wordcloud(content)
+    fig = plt.figure(figsize=(30, 30))
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.tight_layout(pad=0)
 
-st.pyplot(fig)
+    st.pyplot(fig)
