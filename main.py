e1d53078477dfcb8223a6106ef305493c28bb8bd
Rishav Mitra <rishavmitra10@gmail.com>
2022-08-13 12:21:52 +0000

    :sparkles: make it a streamlit app

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
+import streamlit as st
 import wikipedia
 import matplotlib.pyplot as plt
 
 from make_wordcloud import make_wordcloud
 from most_frequent_words import find_most_frequent_words
 
-term = input("Enter a term to look up: ")
+st.write("""
+# Wikipedia article text analysis
+""")
+
+term = st.text_input("Enter a term to look up: ")
 result = wikipedia.search(term, results=1)[0]
 page = wikipedia.page(result)
-print(f"Wordcloud for the Wikipedia page {page.title}")
+
+subheading = f"Wordcloud for the Wikipedia page {page.title}"
+st.write(subheading)
 content = page.content
 
 frequent_words = find_most_frequent_words(content)
 
+words_to_display = ""
+
 for i in frequent_words:
     if i[0].isalnum():
-        print(f"{i[0]}: {i[1]}")
+        words_to_display += f"- {i[0]}: {i[1]}\n"
+
+st.markdown(words_to_display)
 
 wordcloud = make_wordcloud(content)
+fig = plt.figure(figsize=(30, 30))
 plt.imshow(wordcloud)
 plt.axis('off')
 plt.tight_layout(pad=0)
-plt.show()
+
+st.pyplot(fig)
